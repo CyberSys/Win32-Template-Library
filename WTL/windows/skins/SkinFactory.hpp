dfb141973258c939462abb191fd90b4dd25a266b
nick-crowley <nicholas.crowley@gmail.com>
2015-04-29 11:55:16 +0100

    Skeleton skin factory file

---
+++ WTL/windows/skins/SkinFactory.hpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file wtl\windows\skins\SkinFactory.hpp
+//! \brief Provides a look'n'feel class factory for classes to draw window elements
+//! \date 6 March 2015
+//! \author Nick Crowley
+//! \copyright Nick Crowley. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+#ifndef WTL_SKIN_FACTORY_HPP
+#define WTL_SKIN_FACTORY_HPP
+
+#include "wtl/WTL.hpp"
+
+//! \namespace wtl - Windows template library
+namespace wtl
+{
+  
+}
+
+#endif // WTL_SKIN_FACTORY_HPP
