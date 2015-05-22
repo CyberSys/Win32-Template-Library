5a138cd89f7e59acee70d0a8f4f73db7a7c77307
nick-crowley <nicholas.crowley@gmail.com>
2015-05-22 17:26:57 +0100

    added immutable overloads of base_cast

--- WTL/casts/BaseCast.hpp
+++ WTL/casts/BaseCast.hpp
@@ -19,8 +19,8 @@ namespace wtl
   //! 
   //! \tparam T - Any type with a public 'base' member type
   //!
-  //! \param[in] &obj - Object reference
-  //! \return T::base& - Reference to 'obj' base 
+  //! \param[in] &obj - Mutable object reference
+  //! \return T::base& - Mutable reference to 'obj' base 
   //////////////////////////////////////////////////////////////////////////////////////////
   template <typename T, typename BASE = typename T::base>
   BASE& base_cast(T& obj)
@@ -29,13 +29,28 @@ namespace wtl
   };
   
   //////////////////////////////////////////////////////////////////////////////////////////
+  // wtl::base_cast const
+  //! Downcasts a type defining its own base class
+  //! 
+  //! \tparam T - Any type with a public 'base' member type
+  //!
+  //! \param[in] const& obj - Immutable object reference
+  //! \return const T::base& - Immutable reference to 'obj' base 
+  //////////////////////////////////////////////////////////////////////////////////////////
+  template <typename T, typename BASE = typename T::base>
+  const BASE&  base_cast(const T& obj)
+  {
+    return static_cast<const BASE&>(obj);
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////
   // wtl::base_cast
   //! Downcasts a type defining its own base class
   //! 
   //! \tparam T - Any type with a public 'base' member type
   //!
-  //! \param[in] *obj - Object pointer
-  //! \return T::base* - Pointer to 'obj' base 
+  //! \param[in] *obj - Mutable object pointer
+  //! \return T::base* - Mutable pointer to 'obj' base 
   //! 
   //! \throw wtl::invalid_argument - [Debug only] Missing argument
   //////////////////////////////////////////////////////////////////////////////////////////
@@ -47,6 +62,24 @@ namespace wtl
     return static_cast<BASE*>(obj);
   };
   
+  //////////////////////////////////////////////////////////////////////////////////////////
+  // wtl::base_cast const
+  //! Downcasts a type defining its own base class
+  //! 
+  //! \tparam T - Any type with a public 'base' member type
+  //!
+  //! \param[in] const* obj - Object pointer
+  //! \return const T::base* - Pointer to 'obj' base 
+  //! 
+  //! \throw wtl::invalid_argument - [Debug only] Missing argument
+  //////////////////////////////////////////////////////////////////////////////////////////
+  template <typename T, typename BASE = typename T::base>
+  const BASE* base_cast(const T* obj)
+  {
+    REQUIRED_PARAM(obj);
+
+    return static_cast<const BASE*>(obj);
+  };
 }
 
 
