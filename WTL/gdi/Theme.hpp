e03b4c0806e9840aec0f8a3d442c7fa8ab6c6f42
nick-crowley <nicholas.crowley@gmail.com>
2015-11-06 00:56:25 +0000

    Added simple 'Theme' methods and 'HResult' helper wrapper

--- WTL/gdi/Theme.hpp
+++ WTL/gdi/Theme.hpp
@@ -11,6 +11,7 @@
 #include "wtl/WTL.hpp"
 #include "wtl/gdi/DeviceContext.hpp"               //!< HDeviceContext
 #include "wtl/traits/ThemeTraits.hpp"              //!< HTheme
+#include "wtl/platform/HResult.hpp"                //!< HResult
 
 //! \namespace wtl - Windows template library
 namespace wtl
@@ -19,6 +20,8 @@ namespace wtl
   //! \struct Theme - 
   //! 
   //! \tparam ENC - Character encoding 
+  //!
+  //! \remarks See "Parts & States" at https://msdn.microsoft.com/en-us/library/windows/desktop/bb773210(v=vs.85).aspx
   /////////////////////////////////////////////////////////////////////////////////////////
   template <Encoding ENC>
   struct Theme 
@@ -49,17 +52,104 @@ namespace wtl
     //! 
     //! \throw wtl::platform_error - Failed to acquire theme handle
     /////////////////////////////////////////////////////////////////////////////////////////
-    Theme(const HWnd& wnd, const String<Encoding::UTF16>& names)  : Handle(names)
+    Theme(const HWnd& wnd, const String<Encoding::UTF16>& names) : Handle(names)
     {}
     
     // -------------------------------- COPY, MOVE & DESTROY --------------------------------
 
     // ----------------------------------- STATIC METHODS -----------------------------------
-
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // Theme::isActive
+    //! Tests if a visual style for the current application is active.
+    //!
+    //! \return bool - True iff visual style active
+    /////////////////////////////////////////////////////////////////////////////////////////
+    static bool isActive()
+    {
+      return boolean_cast(::IsThemeActive());
+    }
+    
     // ---------------------------------- ACCESSOR METHODS ----------------------------------
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // Theme::drawBackground const
+    //! Draws the border and fill defined by the visual style for the specified control part.
+    //!
+    //! \param[in] const& hdc - Target device context
+    //! \param[in] part - Part to draw
+    //! \param[in] state - State of specified part
+    //! \param[in] const& rc - Drawing rectangle
+    //! 
+    //! \throw wtl::platform_error - Unable to draw background
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename PART, typename STATE>
+    void drawBackground(const HDeviceContext& dc, PART part, STATE state, const RectL& rc) const
+    {
+      if (!HResult(::DrawThemeBackground(Handle, dc, part, state, const_cast<RectL&>(rc), nullptr)))
+        throw platform_error(HERE, "Unable to draw themed control background");
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // Theme::drawBackground const
+    //! Draws the border and fill defined by the visual style for the specified control part.
+    //!
+    //! \param[in] const& hdc - Target device context
+    //! \param[in] part - Part to draw
+    //! \param[in] state - State of specified part
+    //! \param[in] const& rc - Drawing rectangle
+    //! \param[in] const& clip - Clipping rectangle
+    //! 
+    //! \throw wtl::platform_error - Unable to draw background
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename PART, typename STATE>
+    void drawBackground(const HDeviceContext& dc, PART part, STATE state, const RectL& rc, const RectL& clip) const
+    {
+      if (!HResult(::DrawThemeBackground(Handle, dc, part, state, const_cast<RectL&>(rc), const_cast<RectL&>(clip))))
+        throw platform_error(HERE, "Unable to draw themed control background");
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // Theme::drawText const
+    //! Draws text using the color and font defined by the visual style.
+    //!
+    //! \param[in] const& hdc - Target device context
+    //! \param[in] part - Part to draw. If this value is 0, the text is drawn in the default font, or a font selected into the device context.
+    //! \param[in] state - State of specified part
+    //! \param[in] const& txt - Text to draw
+    //! \param[in] const& rc - Drawing rectangle
+    //! \param[in] flags - Drawing flags (Note: DT_CALCRECT is not supported)
+    //! 
+    //! \throw wtl::platform_error - Unable to draw text
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename PART, typename STATE>
+    void drawText(const HDeviceContext& dc, PART part, STATE state, const String<Encoding::UTF16>& str, const RectL& rc, DrawTextFlags flags) const
+    {
+      if (!HResult(::DrawThemeText(Handle, dc, part, state, str.c_str(), str.size(), enum_cast(flags), 0, const_cast<RectL&>(rc))))
+        throw platform_error(HERE, "Unable to draw themed control text");
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // Theme::getContentRect const
+    //! Retrieves the size of the content area for the background defined by the visual style.
+    //!
+    //! \param[in] const& hdc - Target device context
+    //! \param[in] part - Part to query
+    //! \param[in] state - State of specified part
+    //! \param[in] const& rc - Background rectangle
+    //! \param[in,out] & clip - On return this contains the content rectangle
+    //! 
+    //! \throw wtl::platform_error - Unable to query content rectangle
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename PART, typename STATE>
+    void getContentRect(const HDeviceContext& dc, PART part, STATE state, const RectL& rc, RectL& content) const
+    {
+      if (!HResult(::GetThemeBackgroundContentRect(Handle, dc, part, state, const_cast<RectL&>(rc), content)))
+        throw platform_error(HERE, "Unable to query themed control content rectangle");
+    }
 
     // ----------------------------------- MUTATOR METHODS ----------------------------------
-
+    
   };
 
 } // namespace wtl
---
+++ WTL/platform/HResult.hpp
@@ -0,0 +1,129 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//! \file wtl\utils\HResult.hpp
+//! \brief Encapsulates the ::HRESULT type and simplifies its usage
+//! \date 6 November 2015
+//! \author Nick Crowley
+//! \copyright Nick Crowley. All rights reserved.
+//////////////////////////////////////////////////////////////////////////////////////////
+#ifndef WTL_HRESULT_HPP
+#define WTL_HRESULT_HPP
+
+#include "wtl/WTL.hpp"
+#include <wtl\platform\WindowFlags.hpp>       //!< FormatMessageFlags
+#include <wtl\utils\String.hpp>               //!< String
+#include <wtl\utils\Exception.hpp>            //!< platform_error
+#include <wtl\utils\LengthOf.hpp>             //!< lengthof
+
+//! \namespace wtl - Windows template library
+namespace wtl
+{
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \struct HResult - Encapsulates the ::HRESULT type
+  /////////////////////////////////////////////////////////////////////////////////////////
+  struct HResult
+  {
+    // ---------------------------------- TYPES & CONSTANTS ---------------------------------
+    
+    //! \alias type - Define own type
+    using type = HResult;
+	
+    // ----------------------------------- REPRESENTATION -----------------------------------
+  protected:
+    ::HRESULT   Value;      //!< Value
+
+    // ------------------------------------ CONSTRUCTION ------------------------------------
+  public:
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::HResult
+    //! Create from ::HRESULT
+    /////////////////////////////////////////////////////////////////////////////////////////
+    HResult(::HRESULT hr) : Value(hr)
+    {
+    }
+
+    // -------------------------------- COPY, MOVE & DESTROY --------------------------------
+    
+		ENABLE_COPY(HResult);			//!< Can be copied
+		ENABLE_MOVE(HResult);			//!< Can be moved
+		DISABLE_POLY(HResult);	  //!< Cannot be polymorphic 
+		
+    // ----------------------------------- STATIC METHODS -----------------------------------
+    
+    // ---------------------------------- ACCESSOR METHODS ----------------------------------
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::success const
+    //! Query whether value indicates operation succeeded
+    //! 
+    //! \return bool - True iff value indicates operation succeeded
+    /////////////////////////////////////////////////////////////////////////////////////////
+    bool success() const
+    {
+      return SUCCEEDED(Value);
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::check const
+    //! Throw this error as an exception
+    //! 
+    //! \throw wtl::platform_error - Value indicates the operation failed
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /*void check() const
+    {
+      if (FAILED(Value))
+        throw platform_error(Value);
+    }*/
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::toString const
+    //! Formats the error as a string
+    //! 
+    //! \tparam ENC - Character encoding
+    //!
+    //! \return String<ENC> - String representation of value
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <Encoding ENC>
+    String<ENC> toString() const
+    {
+      encoding_char_t<ENC> strError[1024];
+
+      // Lookup system error
+      if (!WinAPI<ENC>::formatMessage(enum_cast(FormatMessageFlags::FromSystem|FormatMessageFlags::IgnoreInserts), nullptr, Value, 0, strError, lengthof(strError), nullptr))
+        return String<ENC>("Unrecognised error code");
+
+      // Assemble exception message and append system error
+      return String<ENC>( strError );
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::operator ! const
+    //! Query whether value indicates operation failed
+    //! 
+    //! \return bool - True iff value indicates operation failed
+    /////////////////////////////////////////////////////////////////////////////////////////
+    bool operator !() const
+    {
+      return FAILED(Value);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // HResult::operator bool const
+    //! Query whether value indicates operation succeeded
+    //! 
+    //! \return bool - True iff value indicates operation succeeded
+    /////////////////////////////////////////////////////////////////////////////////////////
+    /*operator bool() const
+    {
+      return success();
+    }*/
+
+    // ----------------------------------- MUTATOR METHODS ----------------------------------
+
+  };
+}
+
+
+
+#endif // WTL_HRESULT_HPP
+
+
