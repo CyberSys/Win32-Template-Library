8407b4d71e50a2189e65f33d98b82332064f4c32
nick-crowley <nicholas.crowley@gmail.com>
2015-12-01 23:54:10 +0000

    separated msgResult and MessageTraits

---
+++ WTL/platform/MsgResult.hpp
@@ -0,0 +1,104 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//! \file wtl\platform\MsgResult.hpp
+//! \brief 
+//! \date 1 December 2015
+//! \author Nick Crowley
+//! \copyright Nick Crowley. All rights reserved.
+//////////////////////////////////////////////////////////////////////////////////////////
+#ifndef WTL_MESSAGE_RESULT_HPP
+#define WTL_MESSAGE_RESULT_HPP
+
+#include <wtl/WTL.hpp>
+
+//! \namespace wtl - Windows template library
+namespace wtl
+{
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \enum MsgRoute - Defines how a message was routed
+  /////////////////////////////////////////////////////////////////////////////////////////
+  enum class MsgRoute
+  {
+    Handled,      //!< Window handled message
+    Reflected,    //!< Window reflected command/notification to child window
+    Unhandled,    //!< Window ignored message
+  };
+  
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \struct MsgResult - Encapsulates the result and routing of a windows message 
+  //! 
+  //! \tparam RESULT - Message result type
+  /////////////////////////////////////////////////////////////////////////////////////////
+  template <typename RESULT>
+  struct MsgResult
+  {
+    // ---------------------------------- TYPES & CONSTANTS ---------------------------------
+  
+    //! \alias type - Define own type
+    using type = MsgResult<RESULT>;
+  
+    //! \alias result_t - Define result type
+    using result_t = RESULT;
+
+    // ----------------------------------- REPRESENTATION -----------------------------------
+  
+    MsgRoute  Route;        //!< Message routing
+    result_t  Result;       //!< Message result
+
+    // ------------------------------------ CONSTRUCTION ------------------------------------
+	
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //! MsgResult::MsgResult
+    //! Create 'unhandled' result with a default value of -1
+    /////////////////////////////////////////////////////////////////////////////////////////
+    constexpr 
+    MsgResult() noexcept : Route(MsgRoute::Unhandled), Result(-1)
+    {}
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //! MsgResult::MsgResult
+    //! Create 'handled' result from a value
+    //! 
+    //! \param[in] res - Result value
+    /////////////////////////////////////////////////////////////////////////////////////////
+    constexpr 
+    MsgResult(result_t res) noexcept : Route(MsgRoute::Handled), Result(res)
+    {}
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //! MsgResult::MsgResult
+    //! Create result with custom routing and value
+    //! 
+    //! \param[in] route - Routing
+    //! \param[in] res - Result value
+    /////////////////////////////////////////////////////////////////////////////////////////
+    constexpr 
+    MsgResult(MsgRoute route, result_t res) noexcept : Route(route), Result(res)
+    {}
+    
+    // -------------------------------- COPY, MOVE & DESTROY --------------------------------
+  public:
+    CONSTEXPR_COPY_CTOR(MsgResult);      //!< Can be deep copied at compile-time
+    CONSTEXPR_MOVE_CTOR(MsgResult);      //!< Can be moved at compile-time
+    ENABLE_COPY_ASSIGN(MsgResult);       //!< Can be assigned
+    ENABLE_MOVE_ASSIGN(MsgResult);       //!< Can be move-assigned
+    DISABLE_POLY(MsgResult);             //!< Cannot be polymorphic
+
+    // ----------------------------------- STATIC METHODS -----------------------------------
+
+    // ---------------------------------- ACCESSOR METHODS ----------------------------------
+
+    // ----------------------------------- MUTATOR METHODS ----------------------------------
+    
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \alias LResult - Default window message return type
+  /////////////////////////////////////////////////////////////////////////////////////////
+  using LResult = MsgResult<::LRESULT>;
+  
+
+
+} //namespace wtl
+#endif // WTL_MESSAGE_RESULT_HPP
+
---
+++ WTL/traits/MessageTraits.hpp
@@ -0,0 +1,57 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//! \file wtl\traits\MessageTraits.hpp
+//! \brief Provides traits for individual window message types
+//! \date 1 December 2015
+//! \author Nick Crowley
+//! \copyright Nick Crowley. All rights reserved.
+//////////////////////////////////////////////////////////////////////////////////////////
+#ifndef WTL_MESSAGE_TRAITS_HPP
+#define WTL_MESSAGE_TRAITS_HPP
+
+#include <wtl/WTL.hpp>
+#include <wtl/platform/WindowMessage.hpp>     //!< WindowMessage
+#include <wtl/platform/MsgResult.hpp>         //!< MsgRoute
+
+//! \namespace wtl - Windows template library
+namespace wtl
+{
+  
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \struct message_traits - Provides traits for individual window message types
+  //! 
+  //! \tparam T - Window message type
+  /////////////////////////////////////////////////////////////////////////////////////////
+  template <typename T>
+  struct message_traits;
+
+  
+  /////////////////////////////////////////////////////////////////////////////////////////
+  //! \struct message_traits<WindowMessages> - Provides traits for window messages
+  /////////////////////////////////////////////////////////////////////////////////////////
+  template <>
+  struct message_traits<WindowMessage>
+  {
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // wtl::routing 
+    //! Determine whether a message was handled from its result
+    //!
+    //! \param[in] message - Window message 
+    //! \param[in] res - Message result 
+    //! \return bool - True iff result indicates message was unhandled
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename RESULT> 
+    static MsgRoute routing(WindowMessage msg, RESULT res) 
+    {
+      switch (msg)
+      {
+      case WindowMessage::DrawItem:       return res == True ? MsgRoute::Handled : MsgRoute::Unhandled;
+      default:                            return res == 0    ? MsgRoute::Handled : MsgRoute::Unhandled;
+      }
+    }
+  };
+  
+
+} //namespace wtl
+
+#endif // WTL_MESSAGE_TRAITS_HPP
+
