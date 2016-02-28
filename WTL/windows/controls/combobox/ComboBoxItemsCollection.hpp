e5958f91d45d9e02b5edd11911f449ff25ff8749
nick-crowley <nicholas.crowley@gmail.com>
2016-02-28 18:51:36 +0000

    Re-implemented ComboBox items collection so it provides item proxies

--- WTL/windows/controls/combobox/ComboBoxItemsCollection.hpp
+++ WTL/windows/controls/combobox/ComboBoxItemsCollection.hpp
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 //! \file wtl\windows\controls\combobox\ComboBoxItemsCollection.hpp
-//! \brief Abstracts the contents of an edit control as an array of items
+//! \brief Abstracts the contents of an ComboBox control as an array of items
 //! \date 27 February 2016
 //! \author Nick Crowley
 //! \copyright Nick Crowley. All rights reserved.
@@ -21,65 +21,8 @@ namespace wtl
   template <Encoding ENC>
   struct ComboBox;
 
-  
   /////////////////////////////////////////////////////////////////////////////////////////
-  //! \struct ComboBoxItem - 
-  //! 
-  //! \tparam ENC - Character encoding 
-  /////////////////////////////////////////////////////////////////////////////////////////
-  template <Encoding ENC>
-  struct ComboBoxItem 
-  {
-    // ---------------------------------- TYPES & CONSTANTS ---------------------------------
-  
-    //! \alias type - Define own type
-    using type = ComboBoxItem<ENC>;
-    
-    //! \alias char_t - Define character type
-    using char_t = encoding_char_t<ENC>;
-
-    //! \alias resource_t - Define resource id type
-    using resource_t = ResourceId<ENC>;
-    
-    //! \var encoding - Define character encoding
-    static constexpr Encoding encoding = ENC;
-
-    // ----------------------------------- REPRESENTATION -----------------------------------
-  private:
-    String<encoding>   Text;      //!< Item text
-    HIcon              Icon;      //!< Item icon
-    uint32_t           Height;    //!< Item height
-    void*              Data;      //!< Custom data
-  
-    // ------------------------------------ CONSTRUCTION ------------------------------------
-  public:
-    /////////////////////////////////////////////////////////////////////////////////////////
-    // ComboBoxItem::ComboBoxItem
-    //! Create item with icon and text
-    //!
-    //! \param[in] const& txt - Item text
-    //! \param[in] const& icon - Item icon
-    /////////////////////////////////////////////////////////////////////////////////////////
-    template <Encoding InputEncoding>
-    ComboBoxItem(const String<InputEncoding>& txt, const HIcon& icon) : Text(txt), Icon(icon)
-    {}
-
-    DISABLE_COPY(ComboBoxItem);     //!< Prevent removal from collection
-    DISABLE_MOVE(ComboBoxItem);     //!< Prevent removal from collection
-    
-    // -------------------------------- COPY, MOVE & DESTROY --------------------------------
-
-    // ----------------------------------- STATIC METHODS -----------------------------------
-
-    // ---------------------------------- ACCESSOR METHODS ----------------------------------
-
-    // ----------------------------------- MUTATOR METHODS ----------------------------------
-
-  };
-
-
-  /////////////////////////////////////////////////////////////////////////////////////////
-  //! \struct ComboBoxItemsCollection - Abstracts the contents of an edit control as an array of items
+  //! \struct ComboBoxItemsCollection - Abstracts the text of an ComboBox control as an array of items
   //! 
   //! \tparam ENC - Window encoding
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -94,100 +37,190 @@ namespace wtl
     //! \alias char_t - Define window character type
     using char_t = encoding_char_t<ENC>;
     
-    //! \alias window_t - Define owner window type
-    using window_t = ComboBox<ENC>;
+    //! \alias owner_t - Define owner control type
+    using owner_t = ComboBox<ENC>;
 
-    //! \alias value_t - Define item type
-    using value_t = ComboBoxItem<ENC>;
+    /////////////////////////////////////////////////////////////////////////////////////////
+    //! \struct ItemProxy - Proxy for individual items
+    /////////////////////////////////////////////////////////////////////////////////////////
+    struct ItemProxy
+    {
+      // ---------------------------------- TYPES & CONSTANTS ---------------------------------
     
-    //! \alias reference_t - Define mutable reference type
-    using reference_t = value_t&;
+      //! \alias type - Define own type
+      using type = ItemProxy;
+
+      // ----------------------------------- REPRESENTATION -----------------------------------
+    private:
+      const owner_t&  Control;       //!< ComboBox control
+      const uint32_t  ItemIndex;     //!< Zero-based item index
+      
+      // ---------------------------------- CREATE & DESTROY ----------------------------------
+    public:
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::ItemProxy
+      //! Create proxy for a particular item
+      //! 
+      //! \param[in] const& ctrl - ComboBox control
+      //! \param[in] idx - Zero-based item index
+      /////////////////////////////////////////////////////////////////////////////////////////
+      ItemProxy(const owner_t& ctrl, uint32_t idx) : Control(ctrl), 
+                                                     ItemIndex(idx)
+      {}
+
+      // ------------------------------------ COPY & MOVE -------------------------------------
     
-    //! \alias reference_t - Define immutable reference type
-    using const_reference_t = const reference_t;
+      // ----------------------------------- STATIC METHODS -----------------------------------
     
-    //! \var encoding - Inherit character encoding
-    static constexpr Encoding  encoding = base::encoding;
-    
-  private:
-    //! \alias storage_t - Define item storage type
-    using storage_t = std::vector<value_t>;
+      // ---------------------------------- ACCESSOR METHODS ----------------------------------
+    public:
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::get() const &&
+      //! Get item data
+      //! 
+      //! \return T* - Item data
+      //! 
+      //! \throw wtl::platform_error - Unable to retrieve item data
+      /////////////////////////////////////////////////////////////////////////////////////////
+      template <typename T>
+      T* get() const &&
+      {
+        // Query item data
+        switch (uintptr_t data = ComboBox_GetItemData(this->Control.handle(), index()))
+        {
+        case CB_ERR: throw platform_error(HERE, "Unable to query ComboBox item data");
+        default:     return reinterpret_cast<T*>(data);
+        }
+      }
+      
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::height() const
+      //! Query item height
+      //! 
+      //! \return uint32_t - Height of item, in pixels
+      //! 
+      //! \throw wtl::platform_error - Unable to set item height 
+      /////////////////////////////////////////////////////////////////////////////////////////
+      uint32_t height() const &&
+      {
+        // Query item height
+        switch (int32_t h = this->Control.send(ComboBoxMessage::GetItemHeight, index()))
+        {
+        case CB_ERR: throw platform_error(HERE, "Unable to query ComboBox item height");
+        default:     return h;
+        }
+      }
+
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::index() const
+      //! Query item index
+      //! 
+      //! \return uint32_t - Zero-based item index
+      /////////////////////////////////////////////////////////////////////////////////////////
+      uint32_t index() const && noexcept  
+      {
+        return this->ItemIndex;
+      }
+
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::const char_t*() const &&
+      //! Get item data as text
+      //! 
+      //! \return const char_t* - Item text
+      //! 
+      //! \throw wtl::platform_error - Unable to retrieve item data
+      /////////////////////////////////////////////////////////////////////////////////////////
+      operator const char_t*() const &&
+      {
+        return get<const char_t*>();
+      }
+      
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::operator->() const &&
+      //! Get item data as text
+      //! 
+      //! \return const char_t* - Item text
+      //! 
+      //! \throw wtl::platform_error - Unable to retrieve item data
+      /////////////////////////////////////////////////////////////////////////////////////////
+      const char_t* operator->() const &&
+      {
+        return get<const char_t*>();
+      }
+      
+      // ----------------------------------- MUTATOR METHODS ----------------------------------
+      
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::height() 
+      //! Adjust item height
+      //! 
+      //! \param[in] h - Height of item, in pixels
+      //! 
+      //! \throw wtl::platform_error - Unable to set item height
+      /////////////////////////////////////////////////////////////////////////////////////////
+      void height(uint32_t h) &&
+      {
+        if (this->Control.send(ComboBoxMessage::GetItemHeight, index(), h) == CB_ERR)
+          throw platform_error(HERE, "Unable to set height of item ", index(), " to ", h);
+      }
+
+      /////////////////////////////////////////////////////////////////////////////////////////
+      // ItemProxy::operator=
+      //! Set new item data
+      //! 
+      //! \param[in] const* data - Item data
+      //! 
+      //! \throw wtl::platform_error - Unable to set item data
+      /////////////////////////////////////////////////////////////////////////////////////////
+      template <typename ItemData>
+      type& operator=(const ItemData* data) &&
+      {
+        if (ComboBox_SetItemData(this->Control.handle(), index(), data) == CB_ERR)
+          throw platform_error(HERE, "Unable to set data for item ", index());
+      }
+
+      //! Prevent assignment: Cannot assign one proxy to another
+      type& operator=(const type&) = delete;
+    };
+
+    //! \alias proxy_t - Define item proxy type
+    using proxy_t = ItemProxy;
     
     // ----------------------------------- REPRESENTATION -----------------------------------
   private:
-    window_t&  Control;     //!< Associated combobox window
-    storage_t  Items;       //!< Item storage
+    owner_t&  Control;     //!< Associated edit window
 
     // ------------------------------------ CONSTRUCTION ------------------------------------
   public:
     /////////////////////////////////////////////////////////////////////////////////////////
     // ComboBoxItemsCollection::ComboBoxItemsCollection
-    //! Create empty collection and associate with combobox control
+    //! Create collection for ComboBox control
     //! 
     //! \param[in,out] &ctrl - ComboBox control
     /////////////////////////////////////////////////////////////////////////////////////////
-    ComboBoxItemsCollection(window_t& ctrl) : Control(ctrl)
+    ComboBoxItemsCollection(owner_t& ctrl) : Control(ctrl)
     {}
 
     // ---------------------------------- ACCESSOR METHODS ----------------------------------
     
     /////////////////////////////////////////////////////////////////////////////////////////
     // ComboBoxItemsCollection::at const
-    //! Retrieve an element with bounds checking
+    //! Retrieve an item with bounds checking
     //! 
-    //! \param[in] idx - Zero-based line index
-    //! \return const_reference_t - Immutable reference to desired item
+    //! \param[in] idx - Zero-based item index, or -1 for currently selected item
+    //! \return proxy_t - Proxy for specified item
     //! 
-    //! \throw wtl::logic_error - Control does not exist
+    //! \throw wtl::logic_error - ComboBox control does not exist
     //! \throw wtl::out_of_range - Index out of range
     /////////////////////////////////////////////////////////////////////////////////////////
-    const_reference_t at(uint32_t idx) const
+    proxy_t at(int32_t idx) const
     {
-      //! Ensure exists
-      if (!this->Control.exists())
-        throw logic_error(HERE, "ComboBox control does not exist");
-
       // Verify index
-      if (idx >= size())
+      if (idx < -1 || idx >= static_cast<int32_t>(size()))
         throw out_of_range(HERE, "Index ", idx, " is out of range");
 
-      // Lookup item
-      return this->Items[idx];
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////
-    // ComboBoxItemsCollection::append
-    //! Append an item to the collection
-    //! 
-    //! \param[in] idx - Zero-based line index, or -1 for current line
-    //! \return value_t - Specified line text 
-    //! 
-    //! \throw wtl::logic_error - Edit control does not exist
-    //! \throw wtl::out_of_range - Index out of range
-    //! \throw wtl::platform_error - Unable to retrieve window text
-    /////////////////////////////////////////////////////////////////////////////////////////
-    template <typename T>
-    void append(uint32_t idx, const T* obj)
-    {
-      switch (ComboBox_InsertString(this->Control.handle(), idx, obj))
-      {
-      }
-      int i  = CB_ERR, CB_ERRSPACE;
-    }
-
-    /////////////////////////////////////////////////////////////////////////////////////////
-    // ComboBoxItemsCollection::bottom const
-    //! Retrieve the last line of text 
-    //! 
-    //! \return proxy_t - Proxy providing line text
-    //! 
-    //! \throw wtl::logic_error - Edit control does not exist
-    //! \throw wtl::platform_error - Unable to retrieve window text
-    /////////////////////////////////////////////////////////////////////////////////////////
-    proxy_t bottom() const
-    {
-      // Retrieve bottom line
-      return this->operator[](size()-1);
+      // Create proxy for specified item
+      return operator[](idx);
     }
     
     /////////////////////////////////////////////////////////////////////////////////////////
@@ -196,7 +229,7 @@ namespace wtl
     //! 
     //! \return bool - True iff empty
     //! 
-    //! \throw wtl::logic_error - Control does not exist
+    //! \throw wtl::logic_error - ComboBox control does not exist
     /////////////////////////////////////////////////////////////////////////////////////////
     bool empty() const 
     {
@@ -207,67 +240,100 @@ namespace wtl
     // ComboBoxItemsCollection::size() const
     //! Query number of items
     //! 
-    //! \return uint32_t - Number of items, if any
+    //! \return uint32_t - Number of items  
     //! 
-    //! \throw wtl::logic_error - Control does not exist
+    //! \throw wtl::logic_error - ComboBox control does not exist
     /////////////////////////////////////////////////////////////////////////////////////////
     uint32_t size() const 
     {
       //! Ensure exists
       if (!this->Control.exists())
-        throw logic_error(HERE, "ComboBox does not exist");
+        throw logic_error(HERE, "ComboBox control does not exist");
 
-      return this->Items.size();
-      //return ComboBox_GetCount(this->Control.handle());
+      return ComboBox_GetCount(this->Control.handle());
     }
     
     /////////////////////////////////////////////////////////////////////////////////////////
     // ComboBoxItemsCollection::operator[] const
     //! Retrieve an item without bounds checking
     //! 
-    //! \param[in] idx - Zero-based item index
-    //! \return proxy_t - Proxy providing line text
+    //! \param[in] idx - Zero-based item index, or -1 for currently selected item
+    //! \return proxy_t - Proxy for specified item 
     //! 
-    //! \throw wtl::logic_error - Edit control does not exist
-    //! \throw wtl::platform_error - Unable to retrieve window text
+    //! \throw wtl::logic_error - ComboBox control does not exist
     /////////////////////////////////////////////////////////////////////////////////////////
-    value_t operator[](uint32_t idx) const
+    proxy_t operator[](int32_t idx) const
     {
       //! Ensure exists
       if (!this->Control.exists())
-        throw logic_error(HERE, "Edit control does not exist");
+        throw logic_error(HERE, "ComboBox control does not exist");
 
-      // Return item
-      return this->Items[idx];
+      // Create proxy for specified item, otherwise locate currently selected item
+      return proxy_t(this->Control, idx >= 0 ? idx : ComboBox_GetCurSel(this->Control.handle()));
     }
     
     // ----------------------------------- MUTATOR METHODS ----------------------------------
     
     /////////////////////////////////////////////////////////////////////////////////////////
-    // ComboBoxItemsCollection::at 
-    //! Retrieve an element with bounds checking
+    // ComboBoxItemsCollection::append() 
+    //! Appends an item to the collection
     //! 
-    //! \param[in] idx - Zero-based line index
-    //! \return reference_t - Mutable reference to desired item
-    //! 
-    //! \throw wtl::logic_error - Control does not exist
-    //! \throw wtl::out_of_range - Index out of range
+    //! \tparam ItemData - Type of item data
+    //!
+    //! \param[in] const* data - Item text or data
+    //!
+    //! \throw wtl::logic_error - ComboBox control does not exist
+    //! \throw wtl::platform_error - Unable to append item
     /////////////////////////////////////////////////////////////////////////////////////////
-    reference_t at(uint32_t idx) 
+    template <typename ItemData>
+    void append(const ItemData* data) 
     {
-      // Delegate to const implementation
-      return const_cast<reference_t>( static_cast<const type*>(this)->at(idx) );
+      insert(data, -1);
     }
-    
+
     /////////////////////////////////////////////////////////////////////////////////////////
     // ComboBoxItemsCollection::clear() 
     //! Clear all items
+    //!
+    //! \throw wtl::logic_error - ComboBox control does not exist
     /////////////////////////////////////////////////////////////////////////////////////////
-    void clear() const noexcept
+    void clear() const 
     {
-      //! Clear all items
+      //! Ensure exists
+      if (!this->Control.exists())
+        throw logic_error(HERE, "ComboBox control does not exist");
+
+      // Clear items
       ComboBox_ResetContent(this->Control.handle());
     }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // ComboBoxItemsCollection::insert() 
+    //! Inserts an item to the collection at a position
+    //! 
+    //! \tparam ItemData - Type of item data
+    //!
+    //! \param[in] const* data - Item text or data
+    //! \param[in] idx - Zero-based item index at which to insert new item
+    //!
+    //! \throw wtl::logic_error - ComboBox control does not exist
+    //! \throw wtl::platform_error - Unable to append item
+    /////////////////////////////////////////////////////////////////////////////////////////
+    template <typename ItemData>
+    void insert(const ItemData* data, uint32_t idx) 
+    {
+      //! Ensure exists
+      if (!this->Control.exists())
+        throw logic_error(HERE, "ComboBox control does not exist");
+
+      // Append item
+      switch (ComboBox_InsertItemData(this->Control.handle(), idx, data))
+      {
+      case CB_ERR:      throw platform_error(HERE, "Unable to add ComboBox item");
+      case CB_ERRSPACE: throw platform_error(HERE, "Insufficient space to add ComboBox item");
+      }
+    }
+
   };
 
       
